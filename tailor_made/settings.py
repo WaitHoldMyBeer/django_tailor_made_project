b7c09e32a7367d85b4755860cbeea834c11f9cc3
WaitHoldMyBeer <kenanblair2@gmail.com>
2024-09-05 12:13:03 -0700

    Initial commit for Heroku deployment

--- tailor_made/settings.py
+++ tailor_made/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-dwqskri8%&bel$2j71vr+jxlg2k)cob$usrth!+0+1gxx(n&o3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['django-tailor-made-project.herokuapp.com', 'localhost']
 
